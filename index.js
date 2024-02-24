1e776b180bebcec471307ed346e88a87cdf63d90
Mert-Emre <emremertisin@gmail.com>
2024-02-25 02:53:30 +0300

    first commit for production

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
+import authRouter from "./routes/auth.js";
+import userFetchRouter from "./routes/userFetchMatches.js";
+import scoreRoute from "./routes/scoreRoute.js";
+import "dotenv/config";
+
+import connectDb from "./dbConn.js";
+import { client } from "./dbConn.js";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders:
+      "X-Requested-With,X-HTTP-Method-Override,Content-Type,Accept",
+  })
+);
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: [process.env.COOKIE_KEY] }));
+app.use(authRouter);
+app.use(userFetchRouter);
+app.use(scoreRoute);
+
+let db;
+connectDb()
+  .then((res) => {
+    db = res;
+    app.listen(3001, () => {
+      console.log("App is listening on port 3001");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+export { db };
