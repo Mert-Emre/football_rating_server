c5b92fa5d9132877cb3028f6c9fd7ef2462d17af
Mert-Emre <emremertisin@gmail.com>
2024-02-25 06:37:53 +0300

    working on cors

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import connectDb from "./dbConn.js";
 import { client } from "./dbConn.js";
 
 const app = express();
+app.set("trust proxy",1);
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Origin", "https://oyuncupuanla.com");
