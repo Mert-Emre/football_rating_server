ffc731feff9a93236ae36b60149230c732ad666e
Mert-Emre <emremertisin@gmail.com>
2024-02-25 06:16:16 +0300

    working on custom domain

--- index.js
+++ index.js
@@ -11,23 +11,10 @@ import connectDb from "./dbConn.js";
 import { client } from "./dbConn.js";
 
 const app = express();
-app.set("trust proxy",1);
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", "https://football-rating-mei.netlify.app");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-  );
-  next();
-});
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieSession({ keys: [process.env.COOKIE_KEY], sameSite:"none", secure:true, maxAge:24*60*60*1000}));
+app.use(cookieSession({ keys: [process.env.COOKIE_KEY], secure:true, maxAge:24*60*60*1000}));
 app.use(authRouter);
 app.use(userFetchRouter);
 app.use(scoreRoute);
